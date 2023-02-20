693e7d5fc8f87a54e9cfd64075d01a8256c90667
ShohruxNasriddinov <shohshoh1104@gmail.com>
2023-02-21 01:02:40 +0500

    file

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,55 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        // reading the content of file
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            InputStreamReader isr = new InputStreamReader(fileInputStream);
+            BufferedReader br = new BufferedReader(isr);
+//            int ch = fileInputStream.read();
+            String line;
+            while((line = br.readLine()) !=null) {
+                if (line.startsWith("Name: ")){
+                    profile.setName(line.substring(7));
+                }
+                if (line.startsWith("Age: ")){
+                    profile.setAge(Integer.valueOf(line.substring(6)));
+                }
+                if (line.startsWith("Email: ")){
+                    profile.setEmail(line.substring(8));
+                }
+                if (line.startsWith("Phone: ")){
+                    profile.setPhone(Long.valueOf(line.substring(8)));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // exception handling
+        } catch (IOException e) {
+            // exception handling
+        }
+
+        // write the content in file
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            String fileContent ="Name: " + profile.getName();
+            fileOutputStream.write(fileContent.getBytes());
+            fileContent ="Age: " + profile.getAge();
+            fileOutputStream.write(fileContent.getBytes());
+            fileContent ="Email: " + profile.getEmail();
+            fileOutputStream.write(fileContent.getBytes());
+            fileContent ="Phone: " + profile.getPhone();
+            fileOutputStream.write(fileContent.getBytes());
+        } catch (FileNotFoundException e) {
+            // exception handling
+        } catch (IOException e) {
+            // exception handling
+        }
+
+
+        return profile;
     }
 }
